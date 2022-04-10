5686b91ec9b90acac7dd11312e07eecea337f7ad
justintieri <justin@thetieris.com>
2022-04-10 01:54:05 -0500

    WIP: figuring out cosmwasm msg types

---
+++ indexer/actions/cosmwasm.go
@@ -0,0 +1,19 @@
+package actions
+
+/*
+	msg types in wasmd/x/wasm/alias.go
+	MsgStoreCode                   = types.MsgStoreCode
+	MsgStoreCodeResponse           = types.MsgStoreCodeResponse
+	MsgInstantiateContract         = types.MsgInstantiateContract
+	MsgInstantiateContractResponse = types.MsgInstantiateContractResponse
+	MsgExecuteContract             = types.MsgExecuteContract
+	MsgExecuteContractResponse     = types.MsgExecuteContractResponse
+	MsgMigrateContract             = types.MsgMigrateContract
+	MsgMigrateContractResponse     = types.MsgMigrateContractResponse
+	MsgUpdateAdmin                 = types.MsgUpdateAdmin
+	MsgUpdateAdminResponse         = types.MsgUpdateAdminResponse
+	MsgClearAdmin                  = types.MsgClearAdmin
+	MsgWasmIBCCall                 = types.MsgIBCSend
+	MsgClearAdminResponse          = types.MsgClearAdminResponse
+	MsgServer                      = types.MsgServer
+*/
